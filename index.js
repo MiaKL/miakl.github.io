e71558373b31e3fd2768fd80fcc851d5dcc6d8ef
Mia Kelley-Lanser <89313852+MiaKL@users.noreply.github.com>
2024-12-11 00:49:56 -0500

    Updated index.js

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const path = require('path');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.use(express.static(path.join(__dirname, '../frontend')));
+//app.use(express.static(path.join(__dirname, '../frontend')));
+
+app.use(express.static(path.join(process.cwd(),'client','dist', 'frontend', 'index.html')));
+
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/index.html'));
+  //res.sendFile(path.join(__dirname, '../frontend/index.html'));
+  res.sendFile(path.join(process.cwd(),'client','dist','index.html'));
 });
 
 app.listen(port, () => {
