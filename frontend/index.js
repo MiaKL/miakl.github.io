fb3428eeb249b827ce04240ca988ffd1892aaed6
Mia Kelley-Lanser <89313852+MiaKL@users.noreply.github.com>
2024-12-11 00:39:28 -0500

    Updated package.json

--- frontend/index.js
+++ frontend/index.js
@@ -6,7 +6,7 @@ const port = process.env.PORT || 3000;
 app.use(express.static(path.join(__dirname, '../frontend')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, '../frontend/about.html'));
+  res.sendFile(path.join(__dirname, '../frontend/index.html'));
 });
 
 app.listen(port, () => {
