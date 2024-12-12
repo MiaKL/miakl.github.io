ffd79161381b3deb2fb74ef0ede93164c7dd7ece
Mia Kelley-Lanser <89313852+MiaKL@users.noreply.github.com>
2024-12-12 13:41:50 -0500

    Added mobile styling and made logo a button to index

--- script.js
+++ script.js
@@ -7,3 +7,25 @@ document.addEventListener('DOMContentLoaded', function () {
         }, 1000);
     });
 });
+
+const dropdowns = document.querySelectorAll('nav ul li.dropdown');
+    
+    dropdowns.forEach(dropdown => {
+      dropdown.addEventListener('click', function() {
+        this.classList.toggle('active');
+        
+        dropdowns.forEach(otherDropdown => {
+          if (otherDropdown !== dropdown) {
+            otherDropdown.classList.remove('active');
+          }
+        });
+      });
+    });
+
+    document.addEventListener('click', function(event) {
+      if (!event.target.closest('nav ul li.dropdown')) {
+        dropdowns.forEach(dropdown => {
+          dropdown.classList.remove('active');
+        });
+      }
+    });
